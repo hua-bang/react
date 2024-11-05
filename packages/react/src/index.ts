a797f2b314b69e06459a15ac43081103b49c5be2
huahua <huabang001124@gmail.comm>
2024-11-05 22:24:38 +0800

    feat: add react packages

---
+++ packages/react/src/index.ts
@@ -0,0 +1,6 @@
+import { jsx } from "./jsx";
+
+export default {
+  version: '0.0.0',
+  createElement: jsx,
+};
---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,55 @@
+
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import { Type, Key, Ref, Props, ReactElementType, ElementType } from 'shared/ReactTypes';
+
+export const ReactElement = (type: Type, key: Key, ref: Ref, props: Props): ReactElementType => {
+  const element = {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    key,
+    ref,
+    props,
+    __mark: 'hua',
+  };
+
+  return element;
+}
+
+export const jsx = (type: ElementType, config: Record<string, any>, ...maybeChild: any[]) => {
+  let key: Key = null;
+  let ref: Ref = null;
+  const props: Props = {};
+
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === 'key') {
+      if (val !== undefined) {
+        key = '' + val;
+      }
+      continue;
+    }
+
+    if (prop === 'ref') {
+      if (val !== undefined) {
+        ref = val;
+      }
+      continue;
+    }
+
+    if (Object.prototype.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  const maybeChildrenLength = maybeChild.length;
+
+  if (maybeChildrenLength) {
+    if (maybeChildrenLength === 1) {
+      props.children = maybeChild[0];
+    } else {
+      props.children = maybeChild;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};
