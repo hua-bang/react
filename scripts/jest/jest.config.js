3b86e41432d4699bac4431f03807cbd07cf3a0ba
huahua <huabang001124@gmail.comm>
2024-11-28 22:47:33 +0800

    feat: react-noon-render

--- scripts/jest/jest.config.js
+++ scripts/jest/jest.config.js
@@ -3,11 +3,22 @@ module.exports = {
 	...defaults,
 	rootDir: process.cwd(),
 	modulePathIgnorePatterns: ['<rootDir>/.history'],
-	moduleDirectories: [
-		// 对于 React ReactDOM
-		'dist/node_modules',
-		// 对于第三方依赖
-		...defaults.moduleDirectories
-	],
-	testEnvironment: 'jsdom'
+	moduleDirectories: [...defaults.moduleDirectories, 'dist/node_modules'],
+	testEnvironment: 'jsdom',
+	moduleNameMapper: {
+		'^scheduler$': '<rootDir>/node_modules/scheduler/unstable_mock.js'
+	},
+	fakeTimers: {
+		enableGlobally: true,
+		legacyFakeTimers: true
+	},
+	setupFilesAfterEnv: ['./scripts/jest/setupJest.js'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    '/node_modules/(?!your-module-name|another-module).+\\.js$', // 替换为具体模块名称
+  ],
+  
 };
